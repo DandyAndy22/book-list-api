a2fe95248fd5b8c23d346c3ba2215f75b4acc6e6
andras <andrasm96@gmail.com>
2022-02-26 16:52:42 -0800

    Configure API only App
    Switch DB to Postgresql
    Generate resources

---
+++ db/migrate/20220227005129_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.references :author, null: false, foreign_key: true
+      t.references :genre, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
